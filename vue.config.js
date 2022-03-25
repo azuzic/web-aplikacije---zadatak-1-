76695c5101a2f7003835937c22fbec38e8332b88
azuzic <azuzic@student.unipu.hr>
2022-03-25 09:15:22 +0100

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,3 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-
-Vue.use(Vuetify)
-
-export default new Vuetify({
-  theme: { dark: true },
-})
-
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
